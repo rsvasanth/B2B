18bf1e468ac246ab26cca93d9aa140bdfef04688
codearth.inc <rsvasanth@gmail.com>
2017-11-13 15:47:55 +0530

    Init commit

---
+++ client/app/modules/inventory/config/inventory.config.js
@@ -0,0 +1,9 @@
+(function () {
+  'use strict';
+  angular
+    .module('com.module.inventory')
+    .run(function ($rootScope, inventory, gettextCatalog) {
+      // $rootScope.addMenu(gettextCatalog.getString('Browser'), 'app.browser.models', 'fa-globe');
+    });
+
+})();
